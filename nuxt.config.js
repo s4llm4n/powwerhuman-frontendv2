924cca9da361a1c4cc88c465830ea2efc7bf56f3
s4llm4n <s4llm4n@gmail.com>
2022-12-12 23:00:49 +0700

    commit slicing

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/css/main.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -31,6 +33,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxt/postcss8',
     '@nuxtjs/dotenv',
   ],
 
@@ -48,5 +51,12 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+      },
+    },
+  },
 }
